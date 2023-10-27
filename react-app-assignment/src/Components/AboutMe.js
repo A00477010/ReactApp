9a58f076fb1b6285f58bd63dfa1c43fbc1f1e56c
A00477010 <aditya.chaudhari@smu.ca>
2023-10-27 13:26:35 -0300

    Adds Navbar with 2 clickable links About me and My town

---
+++ react-app-assignment/src/Components/AboutMe.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function AboutMe(){
+    return (
+        <div className="userinfo">
+            <h1>Your Name</h1>
+            <p>Some information about me </p>
+            <p>Why ? did I join this MCDA program</p>
+        </div>
+    )
+        
+    
+}
+
+export default AboutMe;
+
+
+
+
---
+++ react-app-assignment/src/Components/MyTown.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MyTown() {
+  return (
+    <div className="userTowninfo">
+      <h1>I live in Hlaifax , NovaScotia</h1>
+      <img src="town-image.jpg" alt="Halifax" />
+      <div id="weather">
+        <p>Temperature:18°C</p>
+        <img src="mild.pg" alt="Mild" />
+      </div>
+    </div>
+  );
+}
+
+export default MyTown;
